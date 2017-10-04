d45867ea1938079c8482bff8cb0e1582521388c1
Makena <roselynemakena@gmail.com>
2017-10-04 15:41:45 +0300

    #increased coverage

--- googs.rb
+++ googs.rb
@@ -16,8 +16,16 @@ def setup
 
 	
 end
+def waiting_time
+
+	wait = Selenium::WebDriver::Wait.new(:timeout => 7)
+
+	
+end
 
 def test_login
+
+	omit("Skip login test")
 
 	username = @my_driver.find_element(:id, "username")
 	username.send_keys("makena@gmail.com")
@@ -28,14 +36,53 @@ def test_login
 	login_btn = @my_driver.find_element(:id, "login_btn")
 
 	login_btn.submit
+	waiting_time
 
 	#Using xpath to get the username and passeord 
 
 	username = @my_driver.find_element(:xpath, '//*[@id="username"]')
 	username.send_keys("UsingXPATH")
+	login_btn = @my_driver.find_element(:id, "login_btn")
+	
+	login_btn.submit
+
+	waiting_time
 
 	# Using an absolute path to obtain an element
-	
+
+	# username = @my_driver.find_element(:css, "html > body > nav > div >ul >li >a")
+	# username.send_keys("Okay CSS is tricky")
+
+	# Using a css selector to select elements
+
+
+
+
+end
+
+def test_radio_links
+	green_radio = @my_driver.find_element(:id, "green")
+	green_radio.click
+
+	red_radio = @my_driver.find_element(:id, "red")
+	puts red_radio.text
+
+	green_radio = @my_driver.find_element(:xpath, '//*[@value="green"]')
+	green_radio.click
+
+	red_radio.click	
+
+	# assertEquals("green", red_radio.text)
+	# assert_equal "green",green_radio.text
+
+	puts "I can print"
+	@my_driver.manage.window.maximize
+
+	# Printing the values of the drop down menu
+	drop_down = @my_driver.find_element(:id, "dropdown_btn")
+
+	puts dropdown.options
+	dropdown.click
 
 
 end
